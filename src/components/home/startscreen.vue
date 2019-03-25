dc8dff93c2c8d7fdacb9f355cb336d9aec68be68
Adeen Shukla <adeen@adeen.me>
2019-03-25 16:23:49 +0530

    Link to Venue on Google Maps (#23)

--- src/components/home/startscreen.vue
+++ src/components/home/startscreen.vue
@@ -26,9 +26,11 @@
                 <p class="mt-2 google-font" style="font-size:120%;color:#424242">{{data.eventDes}}</p>
 
                 <p class="google-font mb-0" style="font-size:120%;color:#757575">
-                    {{data.eventDate}} | {{data.eventTime}} <!--| {{data.eventVenue}}-->
+                    {{data.eventDate}} | {{data.eventTime}} <br /> {{data.eventVenue}}
+                    <v-btn class="mt-0 mx-0 mb-0" icon :href="data.mapUrl" target="_blank">
+                        <i class="fas fa-map-marker-alt"></i>
+                    </v-btn>
                 </p>
-                <p class="google-font mb-0" style="font-size:120%;color:#757575">{{data.eventVenue}}</p>
                 <p class="google-font mt-1" style="font-size:120%;color:#757575">
                     <span v-for="(hs,i) in data.hashTag" :key="i">
                         {{hs.name}} &nbsp;
